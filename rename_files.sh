e8b8a547a8c7c6f5a18a1208090414aba900052f
unknown <joejoebarabanshi@gmail.com>
2021-09-13 16:41:40 +0300

    добавил изменение расширения из больших букв на маленькие

--- rename_files.sh
+++ rename_files.sh
@@ -3,7 +3,26 @@
 #main_path='/mnt/FN05/MB_LS'
 main_path='E:/mosarchiv/mb_ls/'
 
-for i in `find $main_path -type d`
+for folder in `find $main_path -type d`
 do
-  echo $i
+  # попадаем в папку
+  #cd "$main_path""$folder"
+  if [ "$folder" != "$main_path" ]
+  then
+    cd "$folder"
+
+    for file in `ls *`
+    do
+      if [ -d $file ]
+      then
+        echo "is directory"
+      else
+        echo "is file"
+        if [[ $file == *.JPG ]]
+        then
+          mv "$file" "${file%.JPG}.jpg"
+        fi
+      fi
+    done
+  fi
 done
