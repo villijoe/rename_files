78b0381947f10d3abbea92f5cc28646089607e32
unknown <joejoebarabanshi@gmail.com>
2021-09-13 15:31:47 +0300

    добавлена программа для форматирования файла shell и исправлены ошибки цикла

--- rename_files.sh
+++ rename_files.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-main_path='/mnt/FN05/MB_LS'
+#main_path='/mnt/FN05/MB_LS'
+main_path='E:/mosarchiv/mb_ls/'
 
-for i in `find main_path -type d`
+for i in `find $main_path -type d`
 do
   echo $i
 done
